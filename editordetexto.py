c583c6dd185ab68c7d71a9ccf8d849d983e2949b
Cirush <juanigb3@hotmail.com>
2018-02-26 13:17:13 +0100

    Update editordetexto.py

--- editordetexto.py
+++ editordetexto.py
@@ -21,6 +21,8 @@ class MainWindow(QtGui.QMainWindow):
         self.initUI()
     
     def initUI(self):
+        """Se inicializa el nombre del archivo"""
+        self.filename = ""
         """
         Creamos el esqueleto de nuestra aplicacion. 
             -Menu
@@ -36,9 +38,7 @@ class MainWindow(QtGui.QMainWindow):
 
         """Creamos un widget Central"""
         w = QtGui.QWidget()
-        """ Creamos dos botones """
-        okButton = QtGui.QPushButton("Enviar",w)
-        cancelButton = QtGui.QPushButton("Cancelar",w)
+
         """ Creamos un editor de texto """
         self.textLayout = QtGui.QTextEdit(w)
         
@@ -51,7 +51,7 @@ class MainWindow(QtGui.QMainWindow):
             -Tamanio minimo
             -Tamanio por defecto, que sera el recomendado
         """
-        self.setWindowTitle("Aplicacion Basica")
+        self.setWindowTitle("Editor de texto")
         self.setMinimumSize(480,320)
         self.resize(self.sizeHint()) 
 
@@ -70,10 +70,14 @@ class MainWindow(QtGui.QMainWindow):
                 statusTip="Abre un archivo existente", triggered=self.open)
         self.saveAct = QtGui.QAction("&Guardar", self,
                 shortcut="Ctrl+S",
-                statusTip="Guarda el documento en el disco", triggered=self.save)    
+                statusTip="Guarda el documento en el disco", triggered=self.save)   
+        self.saveasAct = QtGui.QAction("&Guardar como", self,
+                shortcut="Ctrl+A",
+                statusTip="Guarda el documento en el disco", triggered=self.saveAs) 
         self.exitAct = QtGui.QAction("&Salir", self, shortcut="Ctrl+Q",
-                statusTip="Salir de la aplicacion", triggered=self.close)    
-        
+                statusTip="Salir de la aplicacion", triggered=self.close)  
+        self.changeFont = QtGui.QAction("&Fuente", self, shortcut="Crtl+F", statusTip="Cambiar el tipo de letra",triggered=self.font)
+        self.list = QtGui.QAction("&Listar", self, shortcut="Crtl+L", statusTip="Crear una lista", triggered=self.lista)
     
     
     def createToolBar(self):
@@ -84,8 +88,10 @@ class MainWindow(QtGui.QMainWindow):
         self.toolbar.addAction(self.newAct)
         self.toolbar.addAction(self.openAct)
         self.toolbar.addAction(self.saveAct)
-        self.toolbar.addAction(self.exitAct)
-
+        self.toolbar.addAction(self.saveasAct)
+        self.formatbar = self.addToolBar('Formato')
+        self.formatbar.addAction(self.changeFont)
+        self.formatbar.addAction(self.list)
   
      
 
@@ -100,10 +106,15 @@ class MainWindow(QtGui.QMainWindow):
         self.fileMenu.addAction(self.newAct)#Añade una acción al menú
         self.fileMenu.addAction(self.openAct)
         self.fileMenu.addAction(self.saveAct)
+        self.fileMenu.addAction(self.saveasAct)
         self.fileMenu.addAction(self.exitAct)
+        self.toolsMenu.addAction(self.changeFont)
+        self.toolsMenu.addAction(self.list)
+        
 
         
-    def newFile(self,w):
+    def newFile(self):
+        self.filename = ""
         self.textLayout.clear()
         self.statusBar().showMessage("Documento creado")
 
@@ -119,12 +130,35 @@ class MainWindow(QtGui.QMainWindow):
 
         
     def save(self):
-        filename = QtGui.QFileDialog.getSaveFileName(self, 'Guardar Documento')
-        f = open(filename, 'w')
-        filedata = self.textLayout.toPlainText()
+        
+        """Si el nombre aún no ha sido puesto se abre un dialogo para elegir
+        el nombre del archivo"""
+        
+        if self.filename == "":
+            self.filename = QtGui.QFileDialog.getSaveFileName(self, 'Guardar Documento')    
+        f = open(self.filename, 'w')
+        filedata = self.textLayout.toHtml()
         f.write(filedata)
         f.close()
         self.statusBar().showMessage("Documento guardado")
+
+    def saveAs(self):
+        self.filename = QtGui.QFileDialog.getSaveFileName(self, 'Guardar Documento')
+        f = open(self.filename, 'w')
+        filedata = self.textLayout.toHtml()
+        f.write(filedata)
+        f.close()
+        self.statusBar().showMessage("Documento guardado")
+    
+    def font(self):
+        font, ok = QtGui.QFontDialog.getFont()
+        if ok:
+            self.textLayout.setFont(font)
+            
+    def lista(self):
+        cursor = self.textLayout.textCursor()
+        cursor.insertList(QtGui.QTextListFormat.ListDisc)
+        
 
 def main():
     
